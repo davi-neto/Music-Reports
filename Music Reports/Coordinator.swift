738de9bdcd6916ce9352b557939bfb84e64c83d9
Davi <davipneto@exxonmobil.com>
2019-08-23 18:24:59 -0300

    Add Coordinator Pattern

---
+++ Music Reports/Coordinator.swift
@@ -0,0 +1,13 @@
+//
+//  Coordinator.swift
+//  Music Reports
+//
+//  Created by Davi Pereira Neto on 23/08/19.
+//  Copyright © 2019 Davi Pereira Neto. All rights reserved.
+//
+
+import UIKit
+
+protocol Coordinator {
+    func start()
+}
